dad33cc1d4089ef86ddc5efbf2ed56fa747a5189
Fady Morris Milad <fadymorris86@gmail.com>
2026-07-16 02:06:28 +0300

    Quickstart: Pagination

--- code/tutorial/settings.py
+++ code/tutorial/settings.py
@@ -126,3 +126,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+
+# Enable pagination
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
