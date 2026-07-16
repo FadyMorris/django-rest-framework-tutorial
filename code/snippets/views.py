56e03f2cdac2ffecd1d5a769a9c2cfda34acb81b
Fady Morris Milad <fadymorris86@gmail.com>
2026-07-16 05:38:09 +0300

    Tutorial 5: Creating an endpoint for the highlighted snippets

--- code/snippets/views.py
+++ code/snippets/views.py
@@ -13,6 +13,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
+from rest_framework import renderers
+
 @api_view(["GET"])
 def api_root(request, format=None):
     return Response(
@@ -43,3 +45,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
